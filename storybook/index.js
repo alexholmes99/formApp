4056e74704c05997032a1276ee86baf70ffb337a
Alex Holmes <90397595+alexholmes99@users.noreply.github.com>
2022-10-26 21:16:49 +0100

    Storybook conditional render (#1)
    * react-native-config
    
    * changed export of storybook
    
    * .env
    
    * conditionally rendering storybook
    
    * add storybook cli
    
    * fixed conditional storybook

--- storybook/index.js
+++ storybook/index.js
@@ -20,12 +20,10 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({
+export const StorybookUIRoot = getStorybookUI({
   asyncStorage: null,
 });
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you should remove this line.
 AppRegistry.registerComponent("%APP_NAME%", () => StorybookUIRoot);
-
-export default StorybookUIRoot;
