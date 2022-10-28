4c93868b209e6d788cc8c1eb2e0dd9cbaa6d1bdb
alexholmes99 <alexh@chillibean.com>
2022-10-28 16:13:53 +0100

    reverting conditional storybook

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { StorybookUIRoot } from "./storybook";
+import { Provider as PaperProvider } from "react-native-paper";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 };
 
-export default process.env.LOAD_STORYBOOK === "true" ? StorybookUIRoot : App;
+export default App;
+// export default StorybookUIRoot;
 
 const styles = StyleSheet.create({
   container: {
